82a13f3621b464ca355692887510f86bf8da4622
chalinx <smile_184@hotmail.com>
2024-06-27 16:02:30 -0500

    Update ProductoController.java

--- src/main/java/uni/isw/Controller/ProductoController.java
+++ src/main/java/uni/isw/Controller/ProductoController.java
@@ -1,7 +1,7 @@
 package uni.isw.Controller;
 
 import java.util.HashMap;
-import java.util.List;//qw
+import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
